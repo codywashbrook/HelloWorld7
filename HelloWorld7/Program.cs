50c7eddd48373983164aa8515e5b11ae94b7d78a
codywashbrook <codywashbrook@hotmail.com>
2022-09-14 13:50:08 -0300

    Initial commit

---
+++ HelloWorld7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World 6!");
+            Console.ReadKey(true);
+        }
+    }
+}
